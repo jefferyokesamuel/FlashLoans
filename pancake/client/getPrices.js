566c5af6fac6ffd89485397c94b5ec6c2ad6e3f6
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 04:10:34 +0100

    Update getPrices.js

---
+++ pancake/client/getPrices.js
@@ -0,0 +1,15 @@
+const ethers = require('ethers')
+
+const { 
+    factoryAddress,
+    addressRouter,
+    addressFrom,
+    addressTo } = require("./addresslist")
+
+const {
+    erc20ABI,
+    factoryABI,
+    pairABI,
+    routerABI } = require("./abiList")
+
+//Standard Provider
