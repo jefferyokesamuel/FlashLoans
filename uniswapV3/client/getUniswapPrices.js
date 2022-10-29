ec15f8f1080d2c735308f0b79b7e49f831d2d915
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 13:15:51 +0100

    Update getUniswapPrices.js

--- uniswapV3/client/getUniswapPrices.js
+++ uniswapV3/client/getUniswapPrices.js
@@ -1,5 +1,15 @@
+//https://docs.uniswap.org/protocol/reference/deployments
+
 const ethers = require('ethers')
 
 const {abi: QuoterABI} = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json')
 
 const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/DFo-cWrsIrwN46dSsl1NIYdrX3jqtxhD")
+
+async function getPrice() {
+    const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
+    const quoterContract = new ethers.Contract(quoterAddress,QuoterABI,provider)
+    console.log(quoterContract)
+}
+
+getPrice()
