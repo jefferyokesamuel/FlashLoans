bc271e1279e04f6613e9111e71be590fe543c085
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 13:28:12 +0100

    Update getUniswapPrices.js

--- uniswapV3/client/getUniswapPrices.js
+++ uniswapV3/client/getUniswapPrices.js
@@ -3,10 +3,11 @@
 const ethers = require('ethers')
 
 const {abi: QuoterABI} = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json')
+const { addressFrom, addressTo } = require('../../uniswapV2/client/client/addresslist')
 
 const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/DFo-cWrsIrwN46dSsl1NIYdrX3jqtxhD")
 
-async function getPrice() {
+async function getPrice(addressFrom, addressTo, readableAmount) {
     const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
     const quoterContract = new ethers.Contract(quoterAddress,QuoterABI,provider)
     // console.log(quoterContract)
@@ -22,5 +23,12 @@ async function getPrice() {
     console.log(quoteExactSingle)
 }
 
+const main = async () => {
+    const addressFrom = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" //USDC
+    const addressTo = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" //WETH
+    const readableAmount = 
+    const amountOut = await getPrice(addressFrom, addressTo)
+    console.log(amountOut);
+}
 
-getPrice()
+main()
