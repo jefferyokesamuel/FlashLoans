031b9ea8a3da6254b029892d3b4b48e9922a4c2f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 14:40:14 +0100

    Update getUniswapPrices.js

--- uniswapV3/client/getUniswapPrices.js
+++ uniswapV3/client/getUniswapPrices.js
@@ -13,16 +13,24 @@ async function getPrice(addressFrom, addressTo, readableAmount) {
 
     const amountIn = ethers.utils.parseUnits(readableAmount, 6)
     const quoteAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-        addressFrom,addressTo,3000,amountIn.toString(),0
+        addressFrom,
+        addressTo,
+        3000,
+        amountIn.toString(),
+        0
     )
-    return quoteAmountOut
+
+    //Output the Amount
+    const amount = ethers.utils.formatUnits(quoteAmountOut.toString(), 18)
+
+    return amount
 }
 
 const main = async () => {
     const addressFrom = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" //USDC
     const addressTo = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" //WETH
-    const readableAmount = "1622"
-    const amountOut = await getPrice(addressFrom, addressTo)
+    const readableAmount = "1629"
+    const amountOut = await getPrice(addressFrom, addressTo, readableAmount)
     console.log(amountOut);
 }
 
