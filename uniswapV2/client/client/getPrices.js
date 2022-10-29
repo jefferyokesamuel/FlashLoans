3d8b93a5a190d118f268c40a198c0ce016e946f3
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 08:36:02 +0100

    Create getPrices.js

---
+++ uniswapV2/client/client/getPrices.js
@@ -0,0 +1,50 @@
+const ethers = require('ethers')
+
+const { 
+    factoryAddress,
+    addressRouter,
+    addressFrom,
+    addressTo } = require("./addresslist")
+
+const {
+    erc20ABI,
+    factoryABI,
+    pairABI,
+    routerABI } = require("./abiList")
+
+//Standard Provider
+const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
+
+//COnnect to Factore
+const contractFactory = new ethers.Contract(factoryAddress,factoryABI,provider)
+
+//Connect to Router
+const contractRouter = new ethers.Contract(addressRouter,routerABI,provider)
+
+//Call the Blockchain
+const getPrices = async (amount) => {
+    //Convert the amount In
+    const contractToken = new ethers.Contract(addressFrom,erc20ABI,provider)
+    const decimals = await contractToken.decimals()
+    const amountIn = ethers.utils.parseUnits(amount, decimals).toString()
+   
+    //Get AMounts Out
+    const amountsOut = await contractRouter.getAmountsOut(amountIn, [
+        addressFrom, //BUSD
+        addressTo, //WBNB
+    ]) 
+
+    //Convert Amount out - decimals
+    const token2 = new ethers.Contract(addressTo,erc20ABI,provider)
+    const decimals2 = await token2.decimals()
+
+     //Convert Amount out - decimals
+    const readableAmount = ethers.utils.formatUnits(amountsOut[1].toString(),decimals)
+
+     //Log Output
+    console.log(readableAmount)
+}
+const amount = "500"
+
+getPrices(amount)
+
