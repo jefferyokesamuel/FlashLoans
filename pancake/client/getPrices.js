7ebb434b5968cab36de1214c04d4ffee0d4ad086
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 04:43:45 +0100

    Update getPrices.js

--- pancake/client/getPrices.js
+++ pancake/client/getPrices.js
@@ -26,7 +26,9 @@ const getPrices = async (amount) => {
     //Convert the amount In
     const contractToken = new ethers.Contract(addressFrom,erc20ABI,provider)
     const decimals = await contractToken.decimals()
+    const amountIn = ethers.utils.parseUnits(amount, decimals)
     console.log(decimals)
+    console.log(amountIn)
 }
 
 const amount = "500"
