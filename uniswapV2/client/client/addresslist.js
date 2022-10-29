e3d746c5b71202f2a79fb7da0dc85bdc19dd9e69
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-29 08:36:05 +0100

    Create addresslist.js

---
+++ uniswapV2/client/client/addresslist.js
@@ -0,0 +1,11 @@
+const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" //WETH
+const addressTo = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" //WBNB
+
+module.exports = {
+    factoryAddress,
+    addressRouter,
+    addressFrom,
+    addressTo
+}
