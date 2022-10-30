c17266e028f90bb099674450d0d1485af99bf0df
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-30 17:22:06 +0100

    Update sendEthtoWETH.js

--- testnetSender/client/sendEthtoWETH.js
+++ testnetSender/client/sendEthtoWETH.js
@@ -6,3 +6,8 @@ const providerTestnet = new ethers.providers.JsonRpcProvider("https://eth-goerli
 //Create Signer
 const address = "0xf6C50D8305fa2AE6986195848053a1437aeD2588"
 const privateKey = "7ca4b79101f50cd1c82d6f8729f98e23b6a652c78fe3c2c492edba51a0b56f40"
+
+const walletSigner = new ethers.Wallet(privateKey, providerTestnet)
+
+console.log(walletSigner)
+
