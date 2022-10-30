432aade56ad875a85e98274f59c90c2c226461e9
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-30 18:43:17 +0100

    Update sendEthtoWETH.js

--- testnetSender/client/sendEthtoWETH.js
+++ testnetSender/client/sendEthtoWETH.js
@@ -22,6 +22,7 @@ const exchangeEth = async () => {
     }
 
     //Send Transaction
+    const transactionSend = await walletSigner.sendTransaction(transactionBuild)
 
     console.log("Transaction Send")
     console.log(transactionSend)
