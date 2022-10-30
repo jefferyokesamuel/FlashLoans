422ae55cdf7e5d46b0a9dbb536ced3b8b8f83059
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-30 17:36:22 +0100

    Update sendEthtoWETH.js

--- testnetSender/client/sendEthtoWETH.js
+++ testnetSender/client/sendEthtoWETH.js
@@ -10,4 +10,7 @@ const privateKey = "7ca4b79101f50cd1c82d6f8729f98e23b6a652c78fe3c2c492edba51a0b5
 const walletSigner = new ethers.Wallet(privateKey, providerTestnet)
 
 console.log(walletSigner)
+        to: "0x60E0DE45a05B3D164d0000B2F12167CDd1660B58",//to
+        nonce: nonce,//nonce
+
 
