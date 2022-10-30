c576578789eec52b99b16a1783193208f3bf3aac
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-30 17:19:45 +0100

    Update sendEthtoWETH.js

--- testnetSender/client/sendEthtoWETH.js
+++ testnetSender/client/sendEthtoWETH.js
@@ -1,4 +1,8 @@
 const { ethers } = require("ethers")
 
 //Testnet Provider
-const providerTestnet = new ethers.providers.JsonRpcProvider("")
+const providerTestnet = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/3g_wiRYgT0GpvrvCJlYfMYWhBSUWGl07")
+
+//Create Signer
+const address = "0xf6C50D8305fa2AE6986195848053a1437aeD2588"
+const privateKey = "7ca4b79101f50cd1c82d6f8729f98e23b6a652c78fe3c2c492edba51a0b56f40"
