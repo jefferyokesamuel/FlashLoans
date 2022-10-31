825334f94fa412012a8a17975768df01faa27931
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-31 06:57:39 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -24,6 +24,6 @@ describe("Read and write to the blockchain", () => {
 
         expect(contractFactory.address).to.equal("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")
 
-        expect(contractFactory.address).to.equal("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")
+        expect(contractRouter.address).to.equal("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
     })
 })
