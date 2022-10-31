707ab52b63ff09fa0dbaa5d419a91754b1e25c51
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-10-31 06:12:42 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -1,2 +1,19 @@
-const { }
+const { ethers } = require('hardhat');
 const { expect } = require("chai");
+
+const { factoryAddress,
+    addressRouter,
+    addressFrom,
+    addressTo } = require("../utils/addressList")
+
+const { erc20ABI, 
+    factoryABI,
+    pairABI,
+    routerABI, } = require("../utils/abiLIst")
+
+describe("read and write to the blockchain", () => {
+    let  provider, contractFactory, contractRouter, contractToken, decimals, amountIn, amountOut 
+
+    provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/DFo-cWrsIrwN46dSsl1NIYdrX3jqtxhD")
+    
+})
