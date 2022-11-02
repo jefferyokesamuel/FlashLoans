07d155817b2df68c645650caf49d7628b27c3c45
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-02 23:53:52 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -39,5 +39,7 @@ describe("Read and write to the blockchain", () => {
 
     it("gets the price of amountsOut", () => {
         const amount = getAmountsOut()
+        console.log(amount)
+        assert()
     })
 })
