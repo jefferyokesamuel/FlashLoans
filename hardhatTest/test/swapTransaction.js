a23bd6e791cb14f60973f30ae0290105e09897ab
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-03 13:25:32 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -48,5 +48,6 @@ describe("Read and write to the blockchain", () => {
     it("Sends a transaction, i.e swaps token", async () => {
         const ownerSigner = await ethers.getSigners()
         console.log(ownerSigner)
+        const myAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
     })
 })
