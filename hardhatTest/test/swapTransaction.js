71abb791576735a8dfbe6e18923766fe3388efc3
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-03 14:14:20 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -54,6 +54,6 @@ describe("Read and write to the blockchain", () => {
 
         const myAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
-        const txSwap = await mainnetRouter.sea
+        const txSwap = await mainnetRouter.swapExactTokensForTokens()
     })
 })
