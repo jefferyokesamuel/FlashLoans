d23c412f0efd4fbe88b2ef313199050e3e1b0c2a
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-07 09:39:52 +0100

    Update hardhat.config.js

--- hardhatTest/hardhat.config.js
+++ hardhatTest/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-waffle")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
