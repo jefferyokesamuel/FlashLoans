c2bb6ac735c4d496fb3783eaaad4876703f3b383
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-07 09:40:11 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -71,6 +71,7 @@ describe("Read and write to the blockchain", () => {
         const { mainnetForkProvider } = waffle.provider
         //  const waff = waffle
         //  console.log(waff)
+        console.log(mainnetForkProvider)
         
         const txRecipt = mainnetForkProvider.getTransactionRecipt(
             txSwap.hash
