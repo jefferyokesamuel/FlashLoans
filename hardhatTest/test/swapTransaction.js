d9e8f04a86777ba4c8c9c5bd575dc2da36714122
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-07 12:24:13 +0100

    Update swapTransaction.js

--- hardhatTest/test/swapTransaction.js
+++ hardhatTest/test/swapTransaction.js
@@ -74,6 +74,7 @@ describe("Read and write to the blockchain", () => {
         const txRecipt = await mainnetForkProvider.getTransactionRecipt(
             txSwap.hash
         )
+        // )
 
         console.log("SWAP TRANSACTION")
         console.log(txSwap)
