5cb56422112a32c0deb8099fc89bacc4bb6931fb
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-11 15:58:21 +0100

    Update hardhat.config.js

--- flashloans/pancakeSwap/hardhat.config.js
+++ flashloans/pancakeSwap/hardhat.config.js
@@ -20,7 +20,9 @@ module.exports = {
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       chainId: 97,
-      accounts: [""],
+      accounts: [
+        "0x7ca4b79101f50cd1c82d6f8729f98e23b6a652c78fe3c2c492edba51a0b56f40"
+      ],
     },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
