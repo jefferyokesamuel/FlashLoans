2b8d4a9ae20f49c9a22b85401d62bed951bd4a2f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-11 23:23:27 +0100

    Update flashSwap.sol

--- flashloans/pancakeSwap/contracts/flashSwap.sol
+++ flashloans/pancakeSwap/contracts/flashSwap.sol
@@ -8,6 +8,7 @@ import "./libraries/UniswapV2Library.sol";
 import "./libraries/SafeERC20.sol";
 import "./interfaces/IUniswapV2Router01.sol";
 import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IUniswapV2Factory.sol";
 
 contract PancakeFlashSwap {
 
