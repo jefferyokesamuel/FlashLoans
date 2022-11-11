eb270d5691e99ffcd639f733bb3a61955208b203
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-11 23:49:08 +0100

    Update flashSwap.sol

--- flashloans/pancakeSwap/contracts/flashSwap.sol
+++ flashloans/pancakeSwap/contracts/flashSwap.sol
@@ -19,4 +19,5 @@ contract PancakeFlashSwap {
     address private constant PANCAKE_FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
     address private constant PANCAKE_ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
 
+    address private constant wBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
 }
