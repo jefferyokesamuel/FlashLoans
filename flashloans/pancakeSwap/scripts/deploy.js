d537a9da0d2d3f825a935fe5331f086a5190a1c8
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-11 15:31:38 +0100

    Update deploy.js

--- flashloans/pancakeSwap/scripts/deploy.js
+++ flashloans/pancakeSwap/scripts/deploy.js
@@ -11,4 +11,5 @@ async function main() {
   const token = await Token.deploy()
 
   console.log("Token address:", token.address)
+main().then(() => process.exit(0)).catch((error) => (
   console.log(error),
