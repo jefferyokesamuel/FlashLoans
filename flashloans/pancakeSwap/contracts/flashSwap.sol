dee8f0b986585dcf8adaa3a33c3de7fa928d650d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-11 23:49:21 +0100

    Update flashSwap.sol

--- flashloans/pancakeSwap/contracts/flashSwap.sol
+++ flashloans/pancakeSwap/contracts/flashSwap.sol
@@ -21,6 +21,7 @@ contract PancakeFlashSwap {
 
     //Token Addresses
     address private constant wBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
     address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
     address private constant USDT = 0x55d398326f99059fF775485246999027B3197955;
     address private constant CROX = 0x2c094F5A7D1146BB93850f629501eB749f6Ed491;
