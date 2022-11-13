165a0c7ef4cd8ac13719b7a8a4de079e84fa28c1
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-14 00:52:40 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -3,3 +3,5 @@ const { expect, assert } = require("chai");
 const { impersonateFundErc20 } = require("../utils/utilities")
 const { abi } = require("../artifacts/contracts/interfaces/IERC20.sol/IERC20.json")
 
+  const BUSD_WHALE = "0xf977814e90da44bfa03b6295a0616a897441acec"
+  const BUSD = 
