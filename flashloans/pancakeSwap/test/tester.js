4c8f497e4d42750dd3ae252f32dc45ec3f0aba97
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-14 00:33:11 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -2,4 +2,7 @@ const {
   time,
   loadFixture,
 const { expect } = require("chai");
+const { ethers } = require('hardhat')
+const { expect, assert } = require("chai");
+const { impersonateFundErc20 } = require("../utils/utilities")
 
