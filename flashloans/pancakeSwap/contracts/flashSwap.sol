0dae0214dc1af50bbfa1861a1dc7ac4f0159e228
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-13 23:25:49 +0100

    Update flashSwap.sol

--- flashloans/pancakeSwap/contracts/flashSwap.sol
+++ flashloans/pancakeSwap/contracts/flashSwap.sol
@@ -79,7 +79,6 @@ contract PancakeFlashSwap {
         require(_sender == address(this), "Sender should match this contract");
 
         //Decode Data to calculate the repayment
-        (address tokenBorrow, uint256 amount) = abi.decode(_data (address, uint256));
         (address tokenBorrow, uint256 amount) = abi.decode(_data, (address, uint256));
 
         //Calculate the amount to repay at the end
