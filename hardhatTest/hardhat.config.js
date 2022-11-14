48b3961262bf486ddfa8eb49e51fddb8b9256012
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-14 01:14:23 +0100

    Update hardhat.config.js

--- hardhatTest/hardhat.config.js
+++ hardhatTest/hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers")
 
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
