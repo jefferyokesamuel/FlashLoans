08e52bc83c81fbd11b3c695e3bcfe10bc630ed2d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-11-14 02:06:42 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -10,7 +10,6 @@ describe('Flash Loan Contract', () => {
   let FLASH_LOAN, BORROW_AMOUNT, FUND_AMOUNT, initiateFundHuman, txArbitrage, gasUsedUSD
   const DECIMALS = 18
 
-  const BUSD_WHALE = "0xf977814e90da44bfa03b6295a0616a897441acec"
   const BUSD_WHALE = "0xbe0eb53f46cd790cd13851d5eff43d12404d33e8"
   const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
   const BNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
