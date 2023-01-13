31a9f8170fdb5ae060707cc772478b585a77cc17
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-01-13 13:16:24 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -4,7 +4,8 @@ const { impersonateFundErc20 } = require("../utils/utilities")
 const { abi, } = require("../artifacts/contracts/interfaces/IERC20.sol/IERC20.json");
 
 const provider  = ethers.provider
-//console.log(provider)
+
+// console.log(provider)
 
 describe('Flash Loan Contract', () => {
   let FLASH_LOAN, BORROW_AMOUNT, FUND_AMOUNT, initialFundingHuman, txArbitrage, gasUsedUSD
