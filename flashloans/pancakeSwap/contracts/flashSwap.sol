36bae07c39855fb88d2747d6b85b4ee2d54c2a3d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-01-14 10:08:05 +0100

    Update flashSwap.sol

--- flashloans/pancakeSwap/contracts/flashSwap.sol
+++ flashloans/pancakeSwap/contracts/flashSwap.sol
@@ -15,11 +15,11 @@ import "./interfaces/IERC20.sol";
 contract PancakeFlashSwap {
     using SafeERC20 for IERC20;
 
-    //Facrory and Routing Address
+    // Facrory and Routing Address
     address private constant PANCAKE_FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
     address private constant PANCAKE_ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
 
-    //Token Addresses
+    // Token Addresses
     address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
     address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
     address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
