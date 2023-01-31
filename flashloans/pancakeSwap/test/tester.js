c5917caf0ea591c9db9469e0f89d7b8708ed24e9
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-01-31 08:43:14 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -7,7 +7,7 @@ const provider  = ethers.provider
 
 //console.log(provider)
 describe('FlashLoan Contract', () => {
-  let FLASHSWAP, BORROW_AMOUNT, FUND_AMOUNT, initiateFundHuman, txArbitrage,  gasUsedUSD
+  let FLASHSWAP, BORROW_AMOUNT, FUND_AMOUNT, initialFundingHuman, txArbitrage,  gasUsedUSD
   const DECIMALS = 18
   const BUSD_WHALE = "0xf977814e90da44bfa03b6295a0616a897441acec"
   const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
