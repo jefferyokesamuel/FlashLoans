12096954a8d0c55dec273a1f20afe067f4c209e2
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-01-31 07:24:27 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -11,6 +11,5 @@ describe('FlashLoan Contract', () => {
   let(FLASHSWAP, BORROW_AMOUNT, FUND_AMOUNT, initiateFundHuman, txArbitrage,  gasUsedUSD)
   const DECIMALS = 18
   const BUSD_WHALE = "0xf977814e90da44bfa03b6295a0616a897441acec"
-  address private const CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
   
 })
