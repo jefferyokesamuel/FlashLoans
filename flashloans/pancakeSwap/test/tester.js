be4a4f028a93037c04b0a3c1c1334009570270a9
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-01 23:34:32 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -65,7 +65,7 @@ describe('FlashLoan Contract', () => {
    })
    it('Executes the Arbitrage', async () => {
       txArbitrage = FLASHSWAP.startArbitrage(BASE_TOKEN_ADDRESS, BORROW_AMOUNT)
-  ;
+  
     
       assert(txArbitrage)
       //Print Balance
