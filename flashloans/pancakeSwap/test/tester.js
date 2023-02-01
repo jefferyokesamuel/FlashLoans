c5830e8076a3f48a87b396f4ca01f2f420197f70
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-01 18:31:07 +0100

    Update tester.js

--- flashloans/pancakeSwap/test/tester.js
+++ flashloans/pancakeSwap/test/tester.js
@@ -19,6 +19,7 @@ describe('FlashLoan Contract', () => {
 
   const tokenbase = new ethers.Contract(BASE_TOKEN_ADDRESS, abi, provider)
 
+    console.log(whale_balance)
   const whale_balance = provider.getBalance(BUSD_WHALE)
   console.log(whale_balance)
   beforeEach(async () => {
